4d6e9cf9f6ce19904751d8038b18fdb6c5a007b2
priyanga-181 <68588551+priyanga-181@users.noreply.github.com>
2021-05-22 00:37:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 # 🚨 Don't change the code below 👇
 tot=0
+number_of_students = 0
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
   tot=tot + student_heights[n]
-avg=tot/len(student_heights)
+ print(f"total height i= {tot}")
+  
+
+for student in student_heights:
+  number_of_students += 1
+print(f"number of students = {number_of_students}")
+  
+  
+avg=tot/number_of_students
 print(round(avg))
 
 
-# 🚨 Don't change the code above 👆
-
-
-
-#Write your code below this row 👇
 
 
 
